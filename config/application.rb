786a153bdf4407ff74c059df96ad26d3115b2d81
Daniel Davey <whoisdanieldavey@gmail.com>
2012-05-05 13:55:33 -0700

    Added exception notifier. Updated app_drone.gem

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,6 @@ module Appdroneapp
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # config.assets.precompile += 'ember/appdroneapp.js'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Appdroneapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.middleware.use ExceptionNotifier,
+    email_prefix: '[Exception] ',
+    sender_address: %{"Drone.bz" <errors@drone.bz>},
+    exception_recipients: %w{whoisdanieldavey@gmail.com}
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
